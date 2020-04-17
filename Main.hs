bd71e2384e00efc19968a6d055499153b080a189
Jisa <jisa0224@gmail.com>
2020-04-17 12:50:03 +0800

    Some code refactoring and remove parser
    no function change

--- Main.hs
+++ Main.hs
@@ -1,7 +1,6 @@
 import System.Environment (getArgs)
-import System.IO (hPutStrLn, stderr)
 import Data.Char (isDigit, isSpace)
-import Data.List (isPrefixOf, stripPrefix, elemIndex)
+import Data.List (isPrefixOf, stripPrefix)
 import Data.Maybe (fromJust)
 
 main :: IO ()
@@ -9,7 +8,7 @@ main = do
     args <- getArgs
     if length args == 1
        then putStr $ compileCtoASM $ head args
-       else hPutStrLn stderr "參數數量錯誤！"
+       else error "參數數量錯誤！"
 
 compileCtoASM :: String -> String
 compileCtoASM input = 
@@ -17,15 +16,15 @@ compileCtoASM input =
      in unlines [".intel_syntax noprefix",
                  ".global main",
                  "main:",
-                 if (not . null) tokens && (isIntegerLiteral $ head tokens)
-                    then "  mov rax, " ++ (show $ (\(IntegerLiteral x) -> x) $ head tokens)
+                 if (not . null) tokens && (isTokenIntegerLiteral $ head tokens)
+                    then "  mov rax, " ++ (show $ (\(TokenIntegerLiteral x) -> x) $ head tokens)
                     else error "必須以數字開頭！",
                  generateASMCodeFromTokens $ tail tokens,
                  "  ret"]
 
 generateASMCodeFromTokens :: [Token] -> String
 generateASMCodeFromTokens [] = ""
-generateASMCodeFromTokens (Operator op:IntegerLiteral num:ts) = 
+generateASMCodeFromTokens (TokenOperator op:TokenIntegerLiteral num:ts) = 
     case op of "+" -> "  add rax, " ++ (show num) ++ "\n"
                "-" -> "  sub rax, " ++ (show num) ++ "\n"
                _ -> error ("Unknown operator \"" ++ op ++ "\"")
@@ -34,13 +33,13 @@ generateASMCodeFromTokens ts = error $ "Cannot generate code from token: " ++ (s
 
 
 -- Tokenizer (Lexical Analysis): String -> Tokens
-data Token = IntegerLiteral Int
-           | Operator String
+data Token = TokenIntegerLiteral Int
+           | TokenOperator String
            deriving (Show, Eq)
 
-isIntegerLiteral :: Token -> Bool
-isIntegerLiteral (IntegerLiteral _) = True
-isIntegerLiteral _ = False
+isTokenIntegerLiteral :: Token -> Bool
+isTokenIntegerLiteral (TokenIntegerLiteral _) = True
+isTokenIntegerLiteral _ = False
 
 tokenize :: String -> [Token]
 tokenize "" = []
@@ -48,11 +47,11 @@ tokenize input
     -- WARNING: isSpace '\n' == True, remember to fix this when handling multi-line source code
     -- WARNING: this will not work for string literals
     | isSpace $ head input = tokenize $ dropWhile isSpace input
-    | isDigit $ head input = (IntegerLiteral $ (read (takeWhile isDigit input) :: Int))
+    | isDigit $ head input = (TokenIntegerLiteral $ (read (takeWhile isDigit input) :: Int))
                              : (tokenize $ dropWhile isDigit input)
     | any (`isPrefixOf` input) operatorList =
         let op = head $ filter (`isPrefixOf` input) operatorList
-         in (Operator op) : (tokenize $ fromJust $ stripPrefix op input)
+         in (TokenOperator op) : (tokenize $ fromJust $ stripPrefix op input)
     | otherwise = error $ "Cannot parse \"" ++ input ++ "\""
     where
         -- WARNING: multi-character operator must precede single-character operator
@@ -67,59 +66,5 @@ tokenize input
 -- term = num | "(" expr ")"
 
 data Node = NodeIntegerLiteral Int
-          | NodeAdd Node Node
-          | NodeSub Node Node
-          | NodeMul Node Node
-          | NodeDiv Node Node
+          | NodeOperator String Node Node
           deriving (Show)
-
-parse :: [Token] -> Node
-parse tokens = 
-    let (ast, remaining) = expr tokens
-     in if null remaining then ast else error $ "Cannot parse tokens: " ++ (show remaining)
-
--- because + and - are left-associated, and Haskell use recursion instead of iteration,
--- we have to parse from right to left, so the left side will at deeper place in AST
--- example: (1 + 2 + 3 + 4) -> ((1 + 2 + 3) + 4) -> (((1 + 2) + 3) + 4) -> ((((1) + 2) + 3) + 4)
---                              ^lhs          ^rhs
-expr :: [Token] -> (Node, [Token])
-expr ts =
-    let lastOperatorIndex = elemLastIndex True $ map (`elem` [Operator "+", Operator "-"]) ts
-     in if lastOperatorIndex == Nothing
-           then mul ts
-           else 
-               let (lhs, lremaining) = expr $ take (fromJust lastOperatorIndex) ts
-                   op = ts !! (fromJust lastOperatorIndex)
-                   (rhs, rremaining) = mul $ drop ((fromJust lastOperatorIndex) + 1) ts
-                in if null lremaining
-                      then case op of (Operator "+") -> (NodeAdd lhs rhs, rremaining)
-                                      (Operator "-") -> (NodeSub lhs rhs, rremaining)
-                      else error $ "Cannot parse: " ++ (show lremaining)
-
-mul :: [Token] -> (Node, [Token])
-mul ts =
-    let lastOperatorIndex = elemLastIndex True $ map (`elem` [Operator "*", Operator "/"]) ts
-     in if lastOperatorIndex == Nothing
-           then term ts
-           else 
-               let (lhs, lremaining) = mul $ take (fromJust lastOperatorIndex) ts
-                   op = ts !! (fromJust lastOperatorIndex)
-                   (rhs, rremaining) = term $ drop ((fromJust lastOperatorIndex) + 1) ts
-                in if null lremaining
-                      then case op of (Operator "*") -> (NodeMul lhs rhs, rremaining)
-                                      (Operator "/") -> (NodeDiv lhs rhs, rremaining)
-                      else error $ "Cannot parse: " ++ (show lremaining)
-
-term :: [Token] -> (Node, [Token])
-term (IntegerLiteral x:ts) = (NodeIntegerLiteral x, ts)
-term (Operator "(":ts) =
-    let (exprInParenthesis, remaining) = expr ts
-     in if head remaining == Operator ")"
-           then (exprInParenthesis, tail remaining)
-           else error "Cannot find \")\""
-term ts = error $ show ts ++ " is not valid"
-
-elemLastIndex :: Eq a => a -> [a] -> Maybe Int
-elemLastIndex t xs = 
-    let a = elemIndex t $ reverse xs
-     in if a == Nothing then Nothing else Just (length xs - fromJust a - 1)
